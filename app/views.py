7c1b4bc3ccebb96b1176c43300a68d27261dc284
Clarissa N Mangkunegara <>
2017-12-26 16:58:12 +0700

    bisa ngitung distance

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from flask import jsonify
 from flask import Response
 from flask import render_template
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from math import sin, cos, sqrt, atan2, radians
 
 
 
@@ -21,12 +22,12 @@ places = ["Chelsea", "Hell's Kitchen", "Hudson Yards", "Lincoln Square", "Little
 	"Marcus Garvey Park", "Morningside Heights", "Sugar Hill", "Sylvan", "Washington Heights", "Lenox Hill", "Turtle Bay"]
 
 
-longitude = ["40.7465004", "40.7637581", "40.7542652", "40.7738280", "40.737103"
+latitude = ["40.7465004", "40.7637581", "40.7542652", "40.7738280", "40.737103"
 	, "40.796989", "40.747027", "40.793961", "40.816212", "40.787011", "40.810300", "40.795740"
 	,"40.825960", "40.811550", "40.853497", "40.867714", "40.802343", "41.030430", "40.876117",	"40.804430", "40.808956", 
 	"40.827930", "43.196458", "40.841708", "40.766232", "40.754037"]
 
-latitude = ["74.0013737", "73.9918181", "74.0031180", "73.9844722", "73.997955"
+longitude = ["74.0013737", "73.9918181", "74.0031180", "73.9844722", "73.997955"
 	, "73.969707", "73.997757", "73.973326", "73.962039", "73.975368", "73.941600", "73.938921"
 	,"73.949608", "73.946477", "73.937452", "73.921202", "73.951093", "73.712680", "73.910263",	"73.943583", "73.962433", 
 	"73.944065", "75.730463", "73.939355", "73.960231", "73.966841"]
@@ -68,12 +69,38 @@ def result():
 	longitude_var = None
 	for x in range(len(places)):
 		if places[x] == pickupLoc:
-			longitude_var = longitude[x]
+			longitude_var = float(longitude[x])
 	
 	latitude_var = None
 	for x in range(len(places)):
+		if places[x] == pickupLoc:
+			latitude_var = float(latitude[x])
+
+	longitude_var1 = None
+	for x in range(len(places)):
 		if places[x] == dropoffLoc:
-			latitude_var = latitude[x]
+			longitude_var1 = float(longitude[x])
+	
+	latitude_var1 = None
+	for x in range(len(places)):
+		if places[x] == dropoffLoc:
+			latitude_var1 = float(latitude[x])
+
+		# approximate radius of earth in km
+	R = 6373.0
+
+	lat1 = radians(latitude_var)
+	lon1 = radians(longitude_var)
+	lat2 = radians(latitude_var1)
+	lon2 = radians(longitude_var1)
+
+	dlon = lon2 - lon1
+	dlat = lat2 - lat1
+
+	a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+	c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+	distance = format(R * c, '.2f')
 
 	
 	pickupLoc = {'pickupLoc' : pickupLoc}
@@ -82,11 +109,12 @@ def result():
 	tip = {'tip' : tip}
 	toll = {'toll' : toll}
 	timestamp = {'timestamp' : timestamp}
+	distance = {'distance' : distance}
 
 
 	# print("The dropoff location is " + dropoffLoc + ".")
 	return render_template('prediction-result.html', pickupLoc=pickupLoc, dropoffLoc=dropoffLoc, passenger=passenger,
-		 tip=tip, toll=toll, timestamp=timestamp)
+		 tip=tip, toll=toll, timestamp=timestamp, distance=distance)
 	
 @app.errorhandler(404)
 def not_found(e):	
